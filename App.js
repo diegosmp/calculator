8b021c20528b0da8a86a3429361320846afd2963
Diêgo Sampaio <diiegosmp@gmail.com>
2023-01-06 12:56:17 -0300

    Criação de logica e add butões na App.js

--- App.js
+++ App.js
@@ -1,11 +1,89 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Display from './src/components/Display'
+import Buttons from './src/components/Buttons'
+
+const intialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
 
 export default class App extends Component {
+  state = {...intialState}
+
+  addDig = n => {
+    const clearDisplay = this.state.displayValue === '0'
+    || this.state.clearDisplay
+
+    if (n === '.' && !clearDisplay &&this.state.displayValue.includes('.')) {
+      return
+    }
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false})
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({values})
+    }
+  }
+
+  clearMemory = () => {
+    this.setState({...intialState})
+  }
+
+  setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true})
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try {
+        values[0] =
+        eval(`${values[0]} ${this.state.operation} ${values[1]}`) 
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: true,
+        values,
+      })
+
+    }
+  }
 
   render() {
     <SafeAreaView style={styles.container}>
-      <Text>Olá, teste 1</Text>
+      <Display value={this.state.displayValue} />
+      <View style={styles.buttons}>
+        <Buttons label='AC' triple onClick={this.clearMemory} />
+        <Buttons label='/' operation onClick={this.setOperation} />
+        <Buttons label='7' onClick={this.addDig} />
+        <Buttons label='8' onClick={this.addDig} />
+        <Buttons label='9' onClick={this.addDig} />
+        <Buttons label='*' operation onClick={this.setOperation} />
+        <Buttons label='4' onClick={this.addDig} />
+        <Buttons label='5' onClick={this.addDig} />
+        <Buttons label='6' onClick={this.addDig} />
+        <Buttons label='-' operation onClick={this.setOperation} />
+        <Buttons label='1' onClick={this.addDig} />
+        <Buttons label='2' onClick={this.addDig} />
+        <Buttons label='3' onClick={this.addDig} />
+        <Buttons label='+' operation onClick={this.setOperation} />
+        <Buttons label='.' onClick={this.addDig} />
+        <Buttons label='=' operation onClick={this.setOperation} />
+      </View>
     </SafeAreaView>
   }
 }
@@ -13,8 +91,9 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  }
+})
